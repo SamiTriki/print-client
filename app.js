9f9ca5b723db5404ba258380b49ff1a895a17b53
Sami Triki <contact@triki.io>
2016-01-17 18:26:59 +0100

    feat - (files): s3 files

--- app.js
+++ app.js
@@ -1,40 +1,24 @@
 "use strict";
 require('dotenv').load();
-
+var s3 = require('./services/s3files');
 var printer = require('printer');
 var util = require('util');
-var path = require('path');
 var _ = require('lodash');
-var fetch = require('node-fetch');
-var request = require('request');
 var printers = printer.getPrinters();
 var printername = "Simulated_Color_Laser___MacBook_Pro_de_Sami";
-var http = require('http');
-var aws = require('aws-sdk');
-var s3 = new aws.S3();
 var template ="N\nS4\nD15\nq400\nR\nB20,10,0,1,2,30,173,B,\"barcode\"\nP0\n";
-// let params = {
-//     Bucket: 'lpt-commandes',
-//     Key: `paris1/105280/commande_fabrication_${105280}.pdf`
-// };
-let params = {
-    Bucket: 'testnidek',
-    Key: `test.zpl`
-};
-s3.getObject(params, (err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        printer.printDirect({
-            data: data.Body,
-            type: 'PDF',
-            printer: printername,
-            success: (id) => console.log('printed with id ' + id),
-            error: (err) => console.error('error on printing: ' + err)
-        });
-        printZebra(data.Body.toString(), printername);
-    }
-});
+
+s3.getOrderDocument('105280', 'commande_fabrication_', 'pdf')
+.then((file) => {
+    printer.printDirect({
+        data: file,
+        type: 'PDF',
+        printer: printername,
+        success: (id) => console.log('printed with id ' + id),
+        error: (err) => console.error('error on printing: ' + err)
+    });
+})
+.catch(console.log('error when trying to get file'));
 
 // function printZebra(barcode_text, printer_name){
 //     printer.printDirect({data:template.replace(/barcode/, barcode_text)
@@ -46,77 +30,3 @@ s3.getObject(params, (err, data) => {
 //         , error:function(err){console.log(err);}
 //     });
 // }
-
-// printer.printDirect({
-//     type: 'RAW',
-//     data: 'BJR',
-//     printer: printername, // printer name, if missing then will print to default printer
-//     // , type: 'RAW' // type: RAW, TEXT, PDF, JPEG, .. depends on platform
-//     success:function(jobID){
-//         console.log("sent to printer with ID: " + jobID);
-//     },
-
-//     error:function(err){console.log(err);}
-// });
-
-// fs.readFile(filename, function(err, data){
-//   if(err) {
-//     console.error('err:' + err);
-//     return;
-//   }
-//   console.log('data type is: '+typeof(data) + ', is buffer: ' + Buffer.isBuffer(data));
-//     printer.printDirect({
-//         data: data,
-//         type: 'PDF',
-//         printer: printername,
-//         success: (id) => console.log('printed with id ' + id),
-//         error: (err) => console.error('error on printing: ' + err)
-//     });
-// });
-// fetch(fileUrl)
-// .then(res => res.text())
-// .then(file => {
-//     printer.printDirect({
-//         data: file.toString(),
-//         type: 'PDF',
-//         printer: printername,
-//         success: (id) => console.log('printed with id ' + id),
-//         error: (err) => console.error('error on printing: ' + err)
-//     });
-// });
-// request.get(fileUrl, (err, res, body) => {
-//     // body = new Buffer(body, 'binary');
-//     console.log(res);
-//     if (!err && res.statusCode == 200) {
-//         printer.printDirect({
-//             data: body,
-//             type: 'PDF',
-//             printer: printername,
-//             success: (id) => console.log('printed with id ' + id),
-//             error: (err) => console.error('error on printing: ' + err)
-//         });
-//       console.log('data type is: '+typeof(body) + ', is buffer: ' + Buffer.isBuffer(body));
-
-//     }
-
-// });
-
-// http.get(fileUrl)
-// .on('response', function (response) {
-//     var body = '';
-//     var i = 0;
-//     response.on('data', function (chunk) {
-//         i++;
-//         body += chunk;
-//         console.log('BODY Part: ' + i);
-//     });
-//     response.on('end', function () {
-//         printer.printDirect({
-//             data: body,
-//             type: 'PDF',
-//             printer: printername,
-//             success: (id) => console.log('printed with id ' + id),
-//             error: (err) => console.error('error on printing: ' + err)
-//         });
-//     });
-// });
--- services/s3Files.js
+++ services/s3Files.js
@@ -2,14 +2,18 @@
 require('dotenv').load();
 
 var aws = require ('aws-sdk');
-var s3 = new aws.s3();
+var s3 = new aws.S3();
 var Promise = require('bluebird');
 var config = require('../config');
 
 exports.getOrderDocument = (id, documentName, extension) => {
-	return new Promise((resolve, reject) {
-		let params = config.prod.aws;
-		params.Key = `${params.subPath}/${id}/${documentName}.${extension}`;
+	return new Promise((resolve, reject) => {
+		let params = {
+			Bucket: config.dev.aws.Bucket,
+			Key: `${config.dev.aws.subPath}/${id}/${documentName}${id}.${extension}`
+		};
+
+		console.log(params);
 
 		s3.getObject(params, (err, data) => {
 			if (err) { reject(err); }
