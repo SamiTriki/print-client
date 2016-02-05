0121b4e9ad0c782724864d09ed3ed6ae793d275f
Sami Triki <contact@triki.io>
2016-02-05 21:45:18 +0100

    fix - (config): error handling

--- managers/configuration-manager.js
+++ managers/configuration-manager.js
@@ -5,12 +5,6 @@ const default_config = require('../config/default_config');
 
 var fs = require('fs');
 
-// reset configuration from cli in case of problem
-if (process.argv[1] == 'reset') {
-    console.log('Resetting configuration to use it in the new directory');
-    exports.reset()
-}
-
 exports.set = (type, prop, value, cb) => {
     let config = exports.getSync();
     if (config[type] && config[type][prop]) {
@@ -20,14 +14,14 @@ exports.set = (type, prop, value, cb) => {
     } else {
         throw new Error(`Can't find your property in the config file`);
     }
-    save(config, () => cb());
+    save(config, () => cb && cb());
 };
 
 exports.replace = (newConfig, cb) => {
     try {
-        save(newConfig, () => cb(null, newConfig));
+        save(newConfig, () => cb && cb(null, newConfig));
     } catch (e) {
-        cb(e);
+        cb && cb(e);
     }
 };
 
@@ -35,12 +29,12 @@ exports.get = (cb) => {
     try {
         fs.readFile(config_file_path, 'utf-8', (err, file) => {
             if (err) { throw new Error(`Failed to read config file, ${err}`); }
-            cb(JSON.parse(file));
+            cb && cb(JSON.parse(file));
         });
     } catch (e) {
         // if no config file, save and return default
         save(default_config,() => {
-            cb(default_config);
+            cb && cb(default_config);
         });
     }
 };
@@ -56,17 +50,23 @@ exports.getSync = () => {
 
 exports.reset = (cb) => {
     try {
-        exports.replace(default_config, () => cb(null, default_config));
+        exports.replace(default_config, () => cb && cb(null, default_config));
     } catch (e) {
-        cb(e);
+        cb && cb(e);
     }
 };
 
 function save (config, cb) {
     fs.writeFile(config_file_path, JSON.stringify(config), (err) => {
-        if (err) { throw new Error('Failed to write config file'); }
+            if (err) { throw new Error(`Failed to write config file, ${err}`); }
         cb && cb();
     });
 }
 
+// reset configuration from cli in case of problem
+if (process.argv.indexOf('reset') > -1) {
+    exports.reset(() => {
+        console.log('Resetting configuration');
+    });
+}
 
